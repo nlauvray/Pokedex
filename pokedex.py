d0748521c350ea5cad34e7f9366abd0bc51fd0d1
Th4End <eliazidb@gmail.com>
2025-01-20 18:31:31 +0100

    test

--- pokedex.py
+++ pokedex.py
@@ -35,5 +35,59 @@ def fight():
         return jsonify({"message": "Fight started!"})
     return render_template('fight.html')
 
+@app.route('/fight/start_battle', methods=['POST'])
+def start_battle():
+    data = request.get_json()
+    player_pokemon = data['player_team'][0]  # Example: Start with the first Pokemon
+    opponent_pokemon = data['opponent_team'][0]  # Example: Start with the first opponent Pokemon
+
+    # Mock data
+    player_data = {
+        'name': player_pokemon,
+        'image': f'/static/{player_pokemon}.png',
+        'abilities': ['Blaze', 'Solar Power'],
+        'types': ['Fire'],
+        'stats': 'HP: 39, Attack: 52, Defense: 43',
+        'attacks': ['Tackle', 'Ember', 'Leer']
+    }
+    opponent_data = {
+        'name': opponent_pokemon,
+        'image': f'/static/{opponent_pokemon}.png',
+        'abilities': ['Overgrow'],
+        'types': ['Water'],
+        'stats': 'HP: 44, Attack: 48, Defense: 65',
+        'attacks': ['Tackle', 'Water Gun']
+    }
+
+    return jsonify({
+        'message': 'Battle started!',
+        'player_image': player_data['image'],
+        'opponent_image': opponent_data['image'],
+        'player_abilities': ', '.join(player_data['abilities']),
+        'player_types': ', '.join(player_data['types']),
+        'player_stats': player_data['stats'],
+        'player_attacks': ', '.join(player_data['attacks']),
+        'opponent_abilities': ', '.join(opponent_data['abilities']),
+        'opponent_types': ', '.join(opponent_data['types']),
+        'opponent_stats': opponent_data['stats'],
+        'opponent_attacks': ', '.join(opponent_data['attacks']),
+    })
+
+@app.route('/fight/battle_turn', methods=['POST'])
+def battle_turn():
+    data = request.get_json()
+    move = data['move']
+
+    # Mock battle logic
+    result = f"{move} used!"
+
+    # Return updated battle state
+    return jsonify({
+        'result': result,
+        'player_stats': 'HP: 30, Attack: 52, Defense: 43',  # Example update
+        'opponent_stats': 'HP: 35, Attack: 48, Defense: 65',  # Example update
+        'battle_over': False
+    })
+
 if __name__ == "__main__":
     app.run(debug=True)
