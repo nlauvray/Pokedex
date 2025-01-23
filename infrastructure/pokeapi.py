7de80ce9a4e6e063bf47a0988a7aad46247d488e
kbellouard <killian.bellouard@getnobullshit.com>
2025-01-23 10:56:20 +0100

    feat(1-appel-apis-et-parsing): error checking

--- infrastructure/pokeapi.py
+++ infrastructure/pokeapi.py
@@ -7,19 +7,31 @@ class PokeApiClient:
     def get_pokemons(self, limit: int = 20, offset: int = 0):
         url = f"{self.base_url}/pokemon?limit={limit}&offset={offset}"
         response = requests.get(url)
+        if response.status_code != 200:
+            raise ValueError("Une erreur est survenu pendant la récupération des pokemons. Veuillez réessayer plus tard.")
+
         return response.json()
 
     def get_pokemon(self, identifier: int or str):
         url = f"{self.base_url}/pokemon/{identifier}"
         response = requests.get(url)
+        if response.status_code != 200:
+            raise ValueError("Une erreur est survenu pendant la récupération du pokemon. Veuillez reéssayer plus tard.")
+
         return response.json()
 
     def get_moves(self, limit: int = 20, offset: int = 0):
         url = f"{self.base_url}/move"
         response = requests.get(url)
+        if response.status_code != 200:
+            raise ValueError("Une erreur est survenu pendant la récupération des moves. Veuillez reéssayer plus tard.")
+
         return response.json()
 
     def get_move(self, identifier: int or str):
         url = f"{self.base_url}/move/{identifier}"
         response = requests.get(url)
+        if response.status_code != 200:
+            raise ValueError("Une erreur est survenu pendant la récupération du move. Veuillez reéssayer plus tard.")
+
         return response.json()
